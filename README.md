78504e58b10824aae18c2308d8a71aa0dd33ab4f
hhenrique <hugohenrique20@gmail.com>
2022-06-30 17:54:16 -0300

    second day

--- README.md
+++ README.md
@@ -6,4 +6,9 @@
 
 #
 
+## Second day - 29/06/2022
+
+***Para o desafio proposto para o segundo dia, implementei "limpeza" do json para deixá-lo conforme eu necessitava afim de obter as listas esperadas. Para isso, implementei com os métodos da classe String, a busca e remoção dos colchetes, depois removi as vírgulas e chaves atribuindo o resultado a um array de string para que cada filme com seus atributos estivesse em uma posição do arrya e assim pudesse manipulá-lo individualmente com o intuíto de buscar os valores dos atributos title, image, year e rating e adicioná-los a uma lista de cada atributo, concluíndo assim o desafio do segundo dia.***
+
+#
 
--- src/sevenDaysOfCode/Main.java
+++ src/sevenDaysOfCode/Main.java
@@ -6,27 +6,81 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 public class Main {
-	
+
 	private static String APIKEY;
+	private static String URLBASE = "https://imdb-api.com/en/API/Top250Movies/";
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
+
 		APIKEY = JOptionPane.showInputDialog("Digite a chave da api IMDb");
-	
-			HttpRequest httpRequest = HttpRequest.newBuilder()
-					.uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + APIKEY))
-					.GET()
-					.build();
-			
-				 HttpResponse<String> response = HttpClient.newHttpClient()
-					     .send(httpRequest, BodyHandlers.ofString()); 
-		
-				 System.out.println(response.body());
-				 
+
+		String json = requestDataMovies(URLBASE, APIKEY);
+
+		String[] jsonArray = parseJson(json);
+
+		/* INICIO - Listas obtidas dos valores dos atributos title, image, year e rating */
+		List<String> listTitleMovies = newList(jsonArray, "title");
+		listTitleMovies.forEach(System.out::println);
+
+		List<String> listUrlImagesMovies = newList(jsonArray, "image");
+		listUrlImagesMovies.forEach(System.out::println);
+
+		List<String> listYearMovies = newList(jsonArray, "year");
+		listYearMovies.forEach(System.out::println);
+
+		List<String> listImDbRatingMovies = newList(jsonArray, "imDbRating");
+		listImDbRatingMovies.forEach(System.out::println);
+		/* FIM - Listas obtidas dos valores dos atributos title, image, year e rating */
+	}
+
+	/* Realizando a requisição e obtendo o json dos top 250 filmes do imdb*/
+	private static String requestDataMovies(String urlApi, String apiKey) throws IOException, InterruptedException {
+
+		HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(urlApi + apiKey)).GET().build();
+
+		HttpResponse<String> response = HttpClient.newHttpClient().send(httpRequest, BodyHandlers.ofString());
+
+		return response.body();
+	}
+
+	/* Retirando os colchetes gerais, chaves e vírgulas entre os filmes */
+	private static String[] parseJson(String json) {
+
+		int indiceColchete1 = json.indexOf("[");
+		int indiceColchete2 = json.indexOf("]");
+		String jsonArrayString = json.substring(indiceColchete1 + 1, indiceColchete2);
+
+		String[] jsonArray = jsonArrayString.split("\\},\\{");
+		jsonArray[0] = jsonArray[0].substring(1);
+
+		int lastIndex = jsonArray.length - 1;
+		int lastChar = jsonArray[lastIndex].indexOf("}");
+
+		jsonArray[lastIndex] = jsonArray[lastIndex].substring(0, lastChar - 1);
+
+		return jsonArray;
+	}
+
+	/* Obtendo as listas de valores dos atributos */
+	private static List<String> newList(String[] jsonArray, String key) {
+
+		List<String> listValues = new ArrayList<>();
+
+		for (int i = 0; i < jsonArray.length; i++) {
+			int lastIndexkey = jsonArray[i].indexOf(key);
+			int indexTwoPoints = jsonArray[i].indexOf(":", lastIndexkey);
+			int indexStartValue = indexTwoPoints + 2;
+			int indexQuotes = jsonArray[i].indexOf("\"", indexStartValue);
+			String titleValue = jsonArray[i].substring(indexStartValue, indexQuotes);
+			listValues.add(titleValue);
+		}
+		return listValues;
 	}
 
 }
