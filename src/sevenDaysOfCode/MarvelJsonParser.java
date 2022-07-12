8beb6e2668db143f58221f4022b025665c31e0ae
hhenrique <hugohenrique20@gmail.com>
2022-07-12 01:20:27 -0300

    refatorando classe de parse de series da Marvel

--- src/sevenDaysOfCode/MarvelJsonParser.java
+++ src/sevenDaysOfCode/MarvelJsonParser.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MarvelJsonParser implements JsonParser{
-	
+public class MarvelJsonParser implements JsonParser {
+
 	private String jsonString;
-	
+
 	public MarvelJsonParser(String jsonString) {
 		this.jsonString = jsonString;
 	}
@@ -16,76 +16,83 @@ public class MarvelJsonParser implements JsonParser{
 	@Override
 	public List<Serie> parse() {
 
-		//exibindo o json após parse
 		String[] array = parseJson(this.jsonString);
-		
+
 		List<Serie> series = new ArrayList<>();
-		
+
 		for (int i = 0; i < array.length; i++) {
-			series.add(new Serie(parseTitle(array[i]), parseUrlImage(array[i]), parseRating(array[i]), parseYear(array[i])));
+			series.add(new Serie(parseTitle(array[i]), parseUrlImage(array[i]), parseRating(array[i]),
+					parseYear(array[i])));
 			series.forEach(System.out::println);
 		}
-		
 		return null;
 	}
-	
+
 	private String parseTitle(String lineJson) {
-		int beginTitle = lineJson.indexOf("title");
-		int endTitle = lineJson.indexOf("\"", beginTitle + 8);
-		String title = lineJson.substring(beginTitle + 8, endTitle).trim();
-		
+		String title = parseAttribute(lineJson, "title");
+
 		return title;
 	}
-	
+
 	private String parseUrlImage(String lineJson) {
-		int beginTitle = lineJson.indexOf("path");
-		int endTitle = lineJson.indexOf("\"", beginTitle + 7);
-		String urlImage = lineJson.substring(beginTitle + 7, endTitle).trim();
+
+		String urlImage = parseAttribute(lineJson, "path");
 		String urlImageFull = urlImage.concat(".").concat(getExtUrlImage(lineJson));
-		
+
 		return urlImageFull;
 	}
-	
+
 	private String getExtUrlImage(String lineJson) {
-		int beginTitle = lineJson.indexOf("extension");
-		int endTitle = lineJson.indexOf("\"", beginTitle + 12);
-		String extension = lineJson.substring(beginTitle + 12, endTitle).trim();
-		
+
+		String extension = parseAttribute(lineJson, "extension");
+
 		return extension;
 	}
-	
+
 	private String parseYear(String lineJson) {
+
 		int beginTitle = lineJson.indexOf("startYear");
 		int endTitle = lineJson.indexOf(",", beginTitle + 11);
 		String year = lineJson.substring(beginTitle + 11, endTitle).trim();
-		
+
 		return year;
 	}
-	
+
 	private String parseRating(String lineJson) {
-		int beginTitle = lineJson.indexOf("rating");
-		int endTitle = lineJson.indexOf("\"", beginTitle + 8);
-		String rating = lineJson.substring(beginTitle + 8, endTitle).trim();
-		
-		if(rating.isEmpty()) rating = "Sem Avaliação";
-		
+
+		String rating = parseAttribute(lineJson, "rating");
+
+		if (rating.isEmpty())
+			rating = "Sem Avaliação";
+
 		return rating;
 	}
-	
+
+	private String parseAttribute(String lineJson, String attribute) {
+
+		Matcher beginAttribute = Pattern.compile(".*\"" + attribute + "\":").matcher(lineJson);
+		beginAttribute.find();
+		int begin = beginAttribute.end();
+		int end = lineJson.indexOf("\"", begin + 1);
+
+		String attributeValue = lineJson.substring(begin + 1, end).trim();
+
+		return attributeValue;
+	}
+
 	private String[] parseJson(String json) {
-		
+
 		Matcher beginArray = Pattern.compile(".*\"results\":\\[").matcher(json);
 		beginArray.find();
 		int begin = beginArray.end();
-		
+
 		Matcher endArray = Pattern.compile(".*\\]}}").matcher(json);
 		endArray.find();
 		int end = endArray.end();
-		
+
 		json = json.substring(begin, end);
-		
 		String[] jsonSerieStrings = json.split("\\},\\{\"id\"");
-		
+
 		return jsonSerieStrings;
 	}
 
